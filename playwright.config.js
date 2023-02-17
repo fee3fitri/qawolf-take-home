140773b89dd1fa1d4361f5bf1175568077e39b24
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2023-02-16 22:57:26 -0800

    Add parameterized test data: invalid email

--- playwright.config.js
+++ playwright.config.js
@@ -5,6 +5,7 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  fullyParallel: true,
 
   /* Configure projects for major browsers */
   projects: [
--- tests/netflix.spec.js
+++ tests/netflix.spec.js
@@ -1,4 +1,4 @@
-const { test, expect, defineConfig } = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
 
 // run tests in headful mode so you can see the browser
 test.use({ headless: false, slowMo: 1000 });
@@ -18,34 +18,74 @@ test("my first test", async ({ page }) => {
 // Email: safitri@myemail.com
 // Password: password
 
+// Parameterized test data
+const invalidEmail = [{
+  email: 'safitri',
+  password: 'password'
+},
+{
+  email: 'saf@myemail.com',
+  password: '1234'
+}];
 
-// 1. Failed log in scenario when email/password combination is invalid
-test('failed log in', async ({ page }) => {
-  // Go to Netflix.com
-  await page.goto("https://www.netflix.com");
 
-  // Click Sign in link
-  await page.getByRole('link', { name: /Sign In/ }).click();
 
-  // Click and fill the Email field with valid email
-  await page.getByText('Email or phone number').click();
-  await page.getByLabel('Email or phone number').fill('safitri');
-
-  // Click and fill the Password field with valid pasword
-  await page.getByText('Password').click();
-  await page.getByLabel('Password').fill('password');
-
-  // Click sign in button
-  await page.getByRole('button', { name: 'Sign In' }).click();
-
-  // Verify that there is error message when log in is failed
-  await expect(page.locator('[data-uia="text"]')).toHaveText(
-    'Sorry, we can\'t find an account with this email address. Please try again or create a new account.'
-  );
+// 1. Failed log in scenario using invalidEmail test data above
+invalidEmail.forEach(data => {
+  test(`failed log in ${data.email}`, async ({ page }) => {
+    // Go to Netflix.com
+    await page.goto("https://www.netflix.com");
+  
+    // Click Sign in link
+    await page.getByRole('link', { name: /Sign In/ }).click();
+  
+    // Click and fill the Email field with valid email
+    await page.getByText('Email or phone number').click();
+    await page.getByLabel('Email or phone number').fill(data.email);
+  
+    // Click and fill the Password field with valid pasword
+    await page.getByText('Password').click();
+    await page.getByLabel('Password').fill(data.password);
+  
+    // Click sign in button
+    await page.getByRole('button', { name: 'Sign In' }).click();
+  
+    // Verify that there is error message when log in is failed
+    await expect(page.locator('[data-uia="text"]')).toHaveText(
+      'Sorry, we can\'t find an account with this email address. Please try again or create a new account.'
+    );
+  });
 });
 
+// 1. Failed log in scenario using invalid password
+invalidEmail.forEach(data => {
+  test(`failed log in ${data.email}`, async ({ page }) => {
+    // Go to Netflix.com
+    await page.goto("https://www.netflix.com");
+  
+    // Click Sign in link
+    await page.getByRole('link', { name: /Sign In/ }).click();
+  
+    // Click and fill the Email field with valid email
+    await page.getByText('Email or phone number').click();
+    await page.getByLabel('Email or phone number').fill('safitri@myemail.com');
+  
+    // Click and fill the Password field with valid pasword
+    await page.getByText('Password').click();
+    await page.getByLabel('Password').fill(data.password);
+  
+    // Click sign in button
+    await page.getByRole('button', { name: 'Sign In' }).click();
+  
+    // Verify that there is error message when log in is failed
+    await expect(page.locator('[data-uia="text"]')).toHaveText(
+      'Sorry, we can\'t find an account with this email address. Please try again or create a new account.'
+    );
+  });
+})
 
-// 2. Successful log in scenario when email/password combination is valid
+
+// 3. Successful log in scenario when email/password combination is valid
 test('successful log in', async ({ page }) => {
   // Go to Netflix.com
   await page.goto("https://www.netflix.com");
